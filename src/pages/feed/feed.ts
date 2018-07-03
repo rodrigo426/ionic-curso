f20abc047f96bcb42407db13fce0693653a493b3
Rodrigo Moura <rodrigomoura426@hotmail.com>
2018-07-03 19:45:26 -0300

    usando json

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,6 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public objeto_feed = {
+    title: "Rodrigo Moura",
+    date: "Julho 3, 2018",
+    description: "Colombia x Inglaterra",
+    qtde_likes: 32,
+    qtde_comments: 4,
+    time_comment: "5h ago"
+  }
   public nome_usuario:string = "Rodrigo"
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
